87e3e08456df0b5ad8cc499c743d0a6b137294db
JanKubat89 <160160201+JanKubat89@users.noreply.github.com>
2024-04-01 10:16:34 +0000

    Vytvoreni TODO informaci

---
+++ Projekt_1.py
@@ -0,0 +1,46 @@
+#TODO   Vytvorit hlavicku
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+author: Jan Kubát
+email: hanys.jan.kubat@gmail.com
+discord: jankubat_13826
+"""
+
+#TODO   Naimportovat zadane soubory
+
+#TODO   Vyzadani si od uzivatele prihlasovaci jmeno a heslo
+
+#TODO   Zjistit, jestli zadane udaje odpovidaji nekomu z registrovanych uzivatelu
+
+#TODO   Pokud je registrovany, pozdrav jej a umozni mu analyzovat texty
+
+#TODO   Pokud neni registrovany, upozorni jej a ukonci program
+
+"""
++------+-------------+
+| user |   password  |
++------+-------------+
+| bob  |     123     |
+| ann  |   pass123   |
+| mike | password123 |
+| liz  |   pass123   |
++------+-------------+
+"""
+
+#TODO   Vytvorit promennou 'TEXTS', ve ktere budou ulozene jednotlive texty
+
+#TODO   Pokud uzivatel vybere takové cislo textu, ktere neni v zadani, program jej upozorni a skonci,
+
+#TODO   Pokud uzivatel zada jiny vstup nez cislo, program jej rovnez upozorni a skonci
+
+#   Pro vybraný text spočítá následující statistiky
+
+#TODO   Pocet slov
+#TODO   pocet slov zacinajicich velkym pismenem
+#TODO   Pocet slov psanych velkymi pismeny
+#TODO   Pocet slov psanych malymi pismeny
+#TODO   Pocet cisel (ne cifer)
+#TODO   Sumu vsech cisel (ne cifer) v textu
+
+#TODO   Vytvorit jednoduchý sloupcovy graf, ktery bude reprezentovat cetnost ruznych delek slov v textu
+

db6e27432017f8d38e772c5060dcc09d1ee7c0d0
JanKubat89 <hanys.jan.kubat@gmail.com>
2024-04-14 17:02:35 +0200

    Added proper username in welcome message.

--- Projekt_1.py
+++ Projekt_1.py
@@ -43,32 +43,31 @@ reg_users = {
 # ------------
 
 TEXTS = [
-    '''Situated about 10 miles west of Kemmerer,
-    Fossil Butte is a ruggedly impressive
-    topographic feature that rises sharply
-    some 1000 feet above Twin Creek Valley
-    to an elevation of more than 7500 feet
-    above sea level. The butte is located just
-    north of US 30N and the Union Pacific Railroad,
-    which traverse the valley.
-    ''',
-    '''At the base of Fossil Butte are the bright
-    red, purple, yellow and gray beds of the Wasatch
-    Formation. Eroded portions of these horizontal
-    beds slope gradually upward from the valley floor
-    and steepen abruptly. Overlying them and extending
-    to the top of the butte are the much steeper
-    buff-to-white beds of the Green River Formation,
-    which are about 300 feet thick.''',
-    '''The monument contains 8198 acres and protects
-    a portion of the largest deposit of freshwater fish
-    fossils in the world. The richest fossil fish deposits
-    are found in multiple limestone layers, which lie some
-    100 feet below the top of the butte. The fossils
-    represent several varieties of perch, as well as
-    other freshwater genera and herring similar to those
-    in modern oceans. Other fish such as paddlefish,
-    garpike and stingray are also present.'''
+'''Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley.''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
 ]
 
 # Kontrola uzivatele a jeho hesla
@@ -82,10 +81,10 @@ TEXTS = [
 # znaku na radek bez toho aniz bych zkratil nazev promenne
 # 'registered_users' na 'reg_users'
 if username in reg_users.keys() and password == reg_users[username]:
-    print('''----------------------------------------
-          Welcome to the app, bob
-          We have 3 texts to be analyzed.
-          ----------------------------------------''')
+    print(f'''----------------------------------------
+Welcome to the app, {username.capitalize()}.
+We have 3 texts to be analyzed.
+----------------------------------------''')
 else:
     print('Unregistered user, terminating the program..')
     exit()
@@ -95,8 +94,8 @@ else:
 """
 if username in registered_users.keys():
     if password == registered_users[username]:
-        print('''----------------------------------------
-              Welcome to the app, bob
+        print(f'''----------------------------------------
+              Welcome to the app, {username.capitalize()}.
               We have 3 texts to be analyzed.
               ----------------------------------------''')
     else:
@@ -119,13 +118,13 @@ print('----------------------------------------')
 
 # Osetreni, ze zadany znak je cokoli jineho nez cislo a ukonceni programu
 if not text_selection_value.isdigit():
-    print('You have not entered a number')
+    print('You have not entered a number.')
     print('Terminating the program..')
     exit()
 
 # Osetreni, ze zadany znak je mimo interval <1, 3> a ukonceni programu
-elif int(text_selection_value) not in range(1, 4):
-    print('You have not entered numbers between 1 to 3')
+if int(text_selection_value) not in range(1, 4):
+    print('You have not entered numbers between 1 to 3.')
     print('Terminating the program..')
     exit()
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Projekt_1
+
+# This basic text analyzer calculates word statistics, identifies case variations (titlecase, uppercase, lowercase), and generates a word length distribution histogram.
